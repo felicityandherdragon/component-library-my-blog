112162ed53cfb5cfeec0ff9921315c8f3110f63b
felicityandherdragon <felicitywuny@gmail.com>
2021-11-04 23:34:29 -0400

    deploy storybook

---
+++ .storybook/myTheme.js
@@ -0,0 +1,37 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  colorPrimary: "hotpink",
+  colorSecondary: "deepskyblue",
+
+  // UI
+  appBg: "white",
+  appContentBg: "silver",
+  appBorderColor: "light",
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "black",
+  textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: "silver",
+  barSelectedColor: "black",
+  barBg: "hotpink",
+
+  // Form colors
+  inputBg: "white",
+  inputBorder: "silver",
+  inputTextColor: "black",
+  inputBorderRadius: 4,
+
+  brandTitle: "A simple style guide",
+  brandUrl: "www.felicityandherdragon.com",
+  brandImage: "xxx"
+});
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import ModalOne from './illustrations/ModalOne.svg';
+import connection from './illustrations/connection.png';
 
 export * from './icons/CloseIcon';
 
 export const Illustrations = {
   ModalOne,
+  connection,
 };
